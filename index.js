7bfbc68140494c234b574332d38289f489edded9
Wesley <wesley.carvalho@jnmoura.com.br>
2019-09-05 19:48:05 -0300

    Desafio 1 - Parte 1

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const server = express();
+server.use(express.json());
+server.listen(3001);
+
+const projects = [];
+
+server.post("/projects", (req, res) => {
+  const project = req.body;
+
+  projects.push(project);
+
+  return res.json(projects);
+});
